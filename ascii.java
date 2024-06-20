5ff6036bed6d5934f25ec3de353d9bebf9da67d3
saik410123 <saik410123@gmail.com>
2024-06-20 17:16:10 +0530

    first file

---
+++ ascii.java
@@ -0,0 +1,10 @@
+class Letters{
+	public static void main(String args[]){
+		char d= 'a',e = 'A';
+		int b = e;
+		if(b>=65 && b<90)
+		System.out.println("It is capital letter");
+		if(b>=97 && b<123)
+		System.out.println("It is small letter");
+		}
+		}
